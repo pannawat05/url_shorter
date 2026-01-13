ac2310fca062f850a7a83951bed336bb636cc6c5
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2026-01-13 13:12:39 +0700

    update

---
+++ front-end/src/lib/components/Alert.svelte
@@ -0,0 +1,51 @@
+<script>
+    export let type = "success";
+    // success | error | warning | info
+
+    export let title = "Alert Title";
+    export let message = "Alert message here";
+    export let linkText = "";
+    export let linkHref = "#";
+
+    export let showActions = true;
+    export let okText = "Ok";
+    export let cancelText = "Cancel";
+
+    export let onOk = () => {};
+    export let onCancel = () => {};
+</script>
+
+<div class={`alert alert-soft alert-${type}`} role="alert">
+    <div>
+        <h3 class="font-semibold">{title}</h3>
+
+        <p class="mt-1">
+            {message}
+            {#if linkText}
+                <a href={linkHref} class="link link-primary font-semibold">
+                    {linkText}
+                </a>
+            {/if}
+        </p>
+    </div>
+
+    {#if showActions}
+        <div class="mt-4 flex gap-2">
+            <button
+                type="button"
+                class="btn btn-primary btn-sm"
+                on:click={onOk}
+            >
+                {okText}
+            </button>
+
+            <button
+                type="button"
+                class="btn btn-outline btn-secondary btn-sm"
+                on:click={onCancel}
+            >
+                {cancelText}
+            </button>
+        </div>
+    {/if}
+</div>
