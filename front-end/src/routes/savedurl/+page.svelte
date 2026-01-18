960d37c34ea5d38537a868d7247a81008774edd0
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2026-01-18 18:27:46 +0700

    Update +page.svelte

--- front-end/src/routes/savedurl/+page.svelte
+++ front-end/src/routes/savedurl/+page.svelte
@@ -65,26 +65,9 @@
 
     function copyToClipboard(text: string, ssid: string) {
         try {
-            if (
-                typeof navigator !== "undefined" &&
-                navigator.clipboard &&
-                typeof navigator.clipboard.writeText === "function" &&
-                window.isSecureContext
-            ) {
-                navigator.clipboard.writeText(
-                    "http://shorturl.panplay-itgoeasy.xyz/" + text,
-                );
-            } else {
-                const textarea = document.createElement("textarea");
-                textarea.value = text;
-                textarea.style.position = "fixed";
-                textarea.style.opacity = "0";
-                document.body.appendChild(textarea);
-                textarea.focus();
-                textarea.select();
-                document.execCommand("copy");
-                document.body.removeChild(textarea);
-            }
+            navigator.clipboard.writeText(
+                "http://shorturl.panplay-itgoeasy.xyz/" + ssid,
+            );
 
             copiedSsid = ssid;
             setTimeout(() => (copiedSsid = null), 2000);
