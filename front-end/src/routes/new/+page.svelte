68c5a166437255ab2bf7db9bfdf788b0c07e91ff
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2026-01-23 15:01:50 +0700

    Update +page.svelte

--- front-end/src/routes/new/+page.svelte
+++ front-end/src/routes/new/+page.svelte
@@ -60,7 +60,7 @@
                 throw new Error(data?.error || "Failed to shorten URL");
             }
 
-            shortenedUrl = data.short_url;
+            shortenedUrl = `http://shorlurl.panplayitgoeasy.xyz/${data.short_url}`;
         } catch (error) {
             console.error(error);
             errorMessage =
